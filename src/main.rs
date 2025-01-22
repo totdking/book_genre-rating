bf63019e79ba7cb02aba40559a124e5c7f4664f5
totdking <timileyindgreat02@gmail.com>
2025-01-22 18:26:54 +0100

    correctly adding the correct files

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,88 @@
-use std::f32::consts::PI;
-fn main() {
-    println!("Hello, world!");
-    let x = Circle{radius: 5.0};
-    let area = x.area();
-    let circ = x.circ();
-    println!("Area of circle is {:#?}", area);
-    println!("Circ of circle is {:#?}", circ);
+use std::io::{self,Write};
+#[derive(Debug)]
+pub enum Genre{
+    Scifi,
+    Romance,
+    Horror,
+    Action,
 }
-struct  Circle{
-    radius : f32,
+#[derive(Debug)]
+struct Book{
+    rating: u32,
+    genre: Genre,
 }
-trait Calculate {
-    fn area(&self) -> f32;
-    fn circ(&self) -> f32;
-}
-impl Calculate for Circle {
-    fn area(&self) -> f32 {
-        PI * self.radius.powf(2.0) as f32
+fn book_rating()-> Result<u32, String>{
+    //LET A RANGE OF RATING FROM 1 TO 5 
+    // let range: u32;
+    loop {
+        println!("What rating range of book are you looking for?");
+        io::stdout().flush().unwrap();
+        let mut rating = String::new();
+        match io::stdin().read_line(&mut rating) {
+            Ok(_) => {
+               match rating.trim().parse::<u32>() {
+                Ok(parsed_rating) =>{
+                    if parsed_rating >= 1 && parsed_rating <= 5 {
+                        break Ok(parsed_rating);
+                    } else {
+                        println!("Please enter a valid rating between 1 and 5.");
+                        continue;
+                    }
+                }
+                Err(_) => {
+                    println!("Please enter a valid number.");
+                    continue;
+                }
+               }
+            }
+            Err(_) =>{ println!("could not read line");
+                        continue;
+                    }
+        }
     }
-    fn circ(&self) -> f32 {
-        2.0 * PI * self.radius as f32
+}
+
+fn book_genre()-> Result<Genre,String>{
+    loop{
+        println!("What genre are you looking for?");
+        println!("Scifi");
+        println!("Romance");
+        println!("Horror");
+        println!("Action");
+        io::stdout().flush().unwrap();
+        let mut genre = String::new();
+        match io::stdin().read_line( &mut genre) {
+            Ok(_) =>{
+                match genre.trim().to_lowercase().as_str(){
+                    "scifi" => return Ok(Genre::Scifi),
+                    "romance" => return Ok(Genre::Romance),
+                    "horror" => return Ok(Genre::Horror),
+                    "action" => return Ok(Genre::Action),
+                    _ => println!("Invalid genre"),
+                }
+                
+            },
+            Err(_) => {
+                println!("Could not read line");
+                continue;
+            }
+        }
     }
+}
+fn main(){
+    let rating =  match book_rating(){
+        Ok(rating) =>{ rating},
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+    let genre =  match book_genre(){
+        Ok(genre) =>{ genre},
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+    println!("Rating: {:?}, Genre: {:?}", rating, genre);
 }
